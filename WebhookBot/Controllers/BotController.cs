38ce00c760e38d07dc51bf16f2adae9d536358c7
Maxim Walter <walter_m@mail.ru>
2017-10-15 11:13:44 +0300

    Add project files.

---
+++ WebhookBot/Controllers/BotController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using WebApplication1.Services;
+
+namespace WebApplication1.Controllers
+{
+    [Route("bot")]
+	public class BotController : Controller
+    {
+        // GET bot/ping
+        [HttpGet]
+        public string Ping()
+        {
+            return "Бот работает. Настройте Ngrok";
+        }
+
+        /// <summary>Метод нужен только для тестирования через Ngrok</summary>
+        // GET bot/Ngrok/hostname
+        [HttpGet("Ngrok/{hostname}")]
+        public RedirectResult Ngrok(string hostname)
+        {
+            BotService.Instance.Connect(hostname);
+            return Redirect(string.Concat("https://", hostname, "/bot"));
+        }
+
+        // POST bot/update
+        [HttpPost]
+		public void Post([FromBody]Update update)
+        {
+            BotService.Instance.Process(update);
+        }
+	}
+}
---
+++ WebhookBot/Services/BotService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace WebApplication1.Services
+{
+    public sealed class BotService
+    {
+        private static readonly Lazy<BotService> instanceHolder =
+            new Lazy<BotService>(() => new BotService());
+        
+        private readonly TelegramBotClient client;
+
+        private BotService()
+        {
+            const string token = "457238275:AAGwAgHUq0i6PwRn2mAYufYp1yHNeJqv4RA";
+            client = new TelegramBotClient(token);
+            //Connect("Сервер_на_которой_работает_сервис");
+        }
+
+        public static BotService Instance
+        {
+            get
+            {
+                return instanceHolder.Value;
+            }
+        }
+
+        /// <summary>Установка связи между ботом и Telegram</summary>
+        public void Connect(string hostname)
+        {
+            client.SetWebhookAsync(string.Concat("https://", hostname, "/bot")).Wait();
+        }
+
+        /// <summary>Установка связи между ботом и Telegram</summary>
+        public void Disconnect()
+        {
+            client.SetWebhookAsync().Wait();
+        }
+        
+        public void Process(Update update)
+        {
+            var message = update.Message;
+
+            if (message.Type == MessageType.TextMessage)
+            {
+                client.SendTextMessageAsync(message.Chat.Id, message.Text).GetAwaiter();
+            }
+        }
+    }
+}
